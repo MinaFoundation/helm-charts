9bf2c41ac3e19c42634760cc8abbf72b2b9a03ac
David Lehuby <david.lehuby@minaprotocol.com>
2023-11-13 10:05:13 +1100

    PM-524 - Mina Daemon - Implement Precomputed block upload on s3

---
+++ mina-daemon/scripts/gcloud-impersonator.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This script is used to impersonate gcloud in the mina-daemon container and store the downloaded blocks in the /blocks directory
+
+echo "gcloud authentication disabled by gcloud-impersonator.sh"
