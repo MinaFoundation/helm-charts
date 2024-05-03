62171c99d0fa28d72e1d4db6d2224dc93f20c663
Smorci <szekelyschmarton@gmail.com>
2024-05-03 15:22:31 +0100

    PM-1570 Refactored cronjob

--- gpt-survey-summarizer/scripts/database-dump-export.sh
+++ gpt-survey-summarizer/scripts/database-dump-export.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
-DATE="$(date +%F_0000)"
+DATE="$(date +%Y_%m_%d_%H_%M)"
 FILENAME="$MINA_PLATFORM-gpt-survey-summarizer-dump-$DATE.rdb"
 
 echo "INFO: Installing tools"
 apk add aws-cli >/dev/null;
 
 echo "INFO: Exporting dump of database"
-redis-cli --pass $REDIS_PASSWORD --rdb $FILENAME
+redis-cli -h gpt-survey-summarizer-redis-master --pass $REDIS_PASSWORD --rdb $FILENAME
 
 echo "INFO: Creating tar archive"
 tar -czvf "$FILENAME.tar.gz" "$FILENAME"
 
 S3_PATH="s3://$S3_BUCKET/$MINA_PLATFORM/$FILENAME.tar.gz"
 echo "INFO: Uploading to S3: $S3_PATH"
-echo aws s3 cp "$FILENAME.tar.gz" "$S3_PATH" 
+aws s3 cp "$FILENAME.tar.gz" "$S3_PATH" 
 
 echo "INFO: Archive database uploaded to bucket"
