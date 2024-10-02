a9397fb7e6c07952389fa03f7431fc7810f73f63
David Lehuby <david.lehuby@minaprotocol.com>
2024-10-02 14:19:35 +1000

    PM-485 - Refactor liveness and readiness for Mina Daemon

---
+++ mina-daemon/scripts/healthcheck.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Function to check mina client status for liveness
+check_liveness() {
+  error_code=$(mina client status --json 2>&1 >/dev/null; echo $?)
+  if [[ $error_code -ne 0 ]] && [[ $error_code -ne 15 ]]; then
+    exit 1
+  fi
+}
+
+# Function to check mina client status for liveness
+check_readiness() {
+  sync_status=$(mina client status --json | jq -r ".sync_status")
+  if [ "$sync_status" != "Synced" ]; then
+    exit 1
+  fi
+}
